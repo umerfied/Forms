fa75ba1553ce22fb473f47ae4426aa8536c229b7
umerfied <momersameer5@gmail.com>
2024-11-30 22:59:18 +0500

    Added the print query

--- action-process.php
+++ action-process.php
@@ -10,14 +10,34 @@
         
         $result = pg_query($conn,$insertquery);
 
-        if($result){
-            $printquery = "SELECT * FROM emails";
-            $print = pg_query($conn,$printquery);
-            echo $print;
+        // if($result){
+        //     $printquery = "SELECT * FROM emails";
+        //     $print = pg_query($conn,$printquery);
+        //     echo $print;
         }
         else{
     
             echo "Failed". preg_last_error($conn);
         }   
-    }    
+    
+    if (!$result) {
+        die("Query failed: " . pg_last_error());
+    }
+    
+    // Display data in an HTML table
+    echo "<table border='1'>";
+    echo "<tr><th>ID</th><th>Email</th></tr>";
+    
+    while ($row = pg_fetch_assoc($result)) {
+        echo "<tr>";
+        echo "<td>" . $row['id'] . "</td>";
+        //echo "<td>" . $row['name'] . "</td>";
+        echo "<td>" . $row['email'] . "</td>";
+        //echo "<td>" . $row['age'] . "</td>";
+        echo "</tr>";
+    }
+    
+    echo "</table>";
+    
+    pg_close($conn);     
         // Your email is <?php echo $_POST["email"]; ?>
